763c2a26e3fbb05587de2334390efd068ae77686
VITOR HUGO MOSCHETTA FARIAS <vitormoschetta@seduc.ro.gov.br>
2020-06-26 01:05:35 -0400

    cv pdf add

--- components/about.js
+++ components/about.js
@@ -8,7 +8,8 @@ Vue.component('about', {
                     <img src="assets/img/lines.svg" class="img-lines" alt="lines">
                     <p>Casado, 31 anos, pai de dois lindos meninos. Fui militar das forças armadas por 8 anos. Gerente comercial por 2 anos e meio.
                     Agora uso meus conhecimentos em análise e desenvolvimento de sistemas.
-                    <a href="http://www.grad.illinois.edu/sites/default/files/pdfs/cvsamples.pdf" class="btn btn-fill" target="_blank" download>Download Curriculo</a>
+                    <a href="documents/cv.pdf" class="btn btn-fill" target="_blank">Download Curriculo</a>
+                  
                 </div>
             </div>
         </div>
