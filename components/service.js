8cc9cdd00b23f45b11d228e98e57b22e157034c5
vitormoschetta <vitormoschetta@gmail.com>
2020-09-16 21:05:13 -0400

    update curriculo e services

--- components/service.js
+++ components/service.js
@@ -4,46 +4,77 @@ Vue.component('service', {
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h3>O que eu faço</h3>
+                    <h2>O que eu faço</h2>
                     <img src="assets/img/lines.svg" class="img-lines" alt="lines">
+
+                    <h4>Análise de Requisitos</h4>
+                    <p>
+                        Entender a necessidade do cliente e encontrar a melhor solução é o primeiro passo para um projeto bem sucedido.<br> 
+                        Existem ferramentas e modelos de desenvolvimento para os mais variados tipos de projeto.
+                        O conhecimento técnico nos permite pensar na solução mais apropriada ainda na fase de análise.
+                    </p>
                 </div>
                 <div class="col-sm-4">
                     <div class="service">
                      <img src="assets/img/consultancy.svg" alt="Coding">
-                        <h4>Análise de Requisitos</h4>
-                        <p>Entender a necessidade do cliente e encontrar a melhor solução é o primeiro passo para um projeto bem sucedido.         
+                        <h4>Aplicação Desktop</h4>
+                        <p>
+                            
                         </p>                                                
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="service">
-                        <img src="assets/img/back-end.svg" alt="Back End Developer">
-                        <h4>Back-end</h4>
-                        <p>O conhecimento técnico nos permite pensar na solução mais apropriada ainda na fase de análise. 
-                        Experiência com Asp.NET Core MVC, Razor Pages e WebAPI. Sólidos conhecimentos nas linguagens C# e SQL. 
-                        Trabalhamos com modernas bibliotecas de persistência de dados, como o Entity Framework (Code e Database First), 
-                        e o Micro ORM Dapper.                     
-                        </p>
+                     <img src="assets/img/consultancy.svg" alt="Coding">
+                        <h4>Aplicação Web</h4>
+                        <p>
+                            Blog / Site / Portal / Web App / Formulário Web
+                        </p>                                                
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="service">
-                        <img src="assets/img/front-end.svg" alt="Front End Developer">
-                        <h4>Front-end</h4>
-                        <p>Entender como as coisas acontecem na tela do usuário também é muito importante. 
-                        Experiência com HTML, CSS, JavaScript e Bibliotecas robustas, como jQuery e Bootstrap.                        
+                     <img src="assets/img/consultancy.svg" alt="Coding">
+                        <h4>Web Services</h4>
+                        <p>
+                            Web API                         
+                        </p>                                                
+                    </div>
+                </div>
+                
+                <div class="col-sm-4">
+                    <div class="service">
+                     <img src="assets/img/consultancy.svg" alt="Coding">
+                        <h4>Aplicação Corporativa</h4>
+                        <p>
+                            Domain Drive Design (DDD) 
+                            <br> / <br>
+                            Aplicação MVC                            
+                        </p>                                                
+                    </div>
+                </div>
+                
+                <!--
+                <div class="col-sm-4">
+                    <div class="service">
+                        <img src="assets/img/back-end.svg" alt="Back End Developer">
+                        <h4>Mobile</h4>
+                        <p>                                                                               
+                            App Multiplataforma                 
                         </p>
                     </div>
                 </div>
+                -->
+                
             </div>
         </div>  
     `
-  })
+})
 
 
-  var service = new Vue({
+var service = new Vue({
     el: '#service',
     data: {
-    
+
     }
-  })
+})
